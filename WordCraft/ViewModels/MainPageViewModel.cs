176f66ba8b8f291ad22002fc4bde9a06c9c4f064
Asim Adalat (adalata) <90917263+asimadalat@users.noreply.github.com>
2024-06-03 22:57:34 +0100

    Import special words from CSV
    Project created, views and viewmodels initialised. Resources folder created with CSV file, of which the 'Word' column is converted into a list using the getSpecialWords() method.

---
+++ WordCraft/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using _6002CEM_Maui_App.ViewModels;
+using System;
+using System.Windows;
+using System.IO;
+using System.Windows.Shapes;
+
+namespace WordCraft.ViewModels
+{
+    public partial class MainPageViewModel : PageViewModel
+    {
+        public MainPageViewModel()
+        {
+            List<string> specials = getSpecialWords();
+            MessageBox.Show(specials[21]);
+        }
+
+        private List<string> getSpecialWords()
+        {
+            List<string> words = new List<string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader("../../../Resources/Words50.csv"))
+                {
+                    if (!reader.EndOfStream)
+                    {
+                        reader.ReadLine(); 
+                    }
+
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //MessageBox.Show((line.Split(','))[0]);
+                        words.Add((line.Split(','))[0]);
+                    }
+                }
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return words;
+        }
+    }
+}
+
---
+++ WordCraft/ViewModels/PageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _6002CEM_Maui_App.ViewModels;
+
+// Base view model, other vms to inherit OnPropertyChanged
+public class PageViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    // For ui element updates 
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public PageViewModel()
+    {
+    }
+}
