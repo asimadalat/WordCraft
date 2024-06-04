1537a04b81c3e70c59655334e22e1dfdc6ec2185
Asim Adalat (adalata) <90917263+asimadalat@users.noreply.github.com>
2024-06-04 12:22:01 +0100

    Word scoring implemented
    Words can now be entered to receive a score. Validation prevents multiple words, null values or words containing characters other than alphabetic letters from receiving a score. Score is double if word is in the special words list, not case sensitive.

--- WordCraft/ViewModels/MainPageViewModel.cs
+++ WordCraft/ViewModels/MainPageViewModel.cs
@@ -2,19 +2,34 @@
 using System;
 using System.Windows;
 using System.IO;
+using System.Linq;
+using CommunityToolkit.Mvvm.Input;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace WordCraft.ViewModels
 {
     public partial class MainPageViewModel : PageViewModel
     {
-        public MainPageViewModel()
-        {
-            List<string> specials = getSpecialWords();
-            MessageBox.Show(specials[21]);
+        private static readonly int[] 
+            letterScores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
+
+        private string _currentWord;
+        public string CurrentWord { get => _currentWord;
+            set {
+                if (_currentWord != value) {
+                    _currentWord = value;
+                    OnPropertyChanged(nameof(CurrentWord));
+                }
+            }
         }
 
-        private List<string> getSpecialWords()
+        public MainPageViewModel()
+        {
+
+        }
+
+        private List<string> GetSpecialWords()
         {
             List<string> words = new List<string>();
             try
@@ -38,6 +53,45 @@ namespace WordCraft.ViewModels
                 MessageBox.Show(e.Message);
             }
             return words;
+        }
+
+        [RelayCommand]
+        public Task CalculateScore()
+        {
+            if (_currentWord == null || _currentWord == "") {
+                MessageBox.Show("Please enter a word.", "Cannot submit blank");
+            } else if (_currentWord.Contains(" ")) {
+                MessageBox.Show("Please enter only a single word.", "Cannot submit multiple words");
+            } else if (!_currentWord.All(Char.IsLetter)) {
+                MessageBox.Show("Please enter a word consisting of letters of the alphabet.", "No numeric or special characters");
+            } else
+            {
+                try
+                {
+
+                    int scoreCounter = 0;
+                    foreach (char c in _currentWord)
+                    {
+                        scoreCounter += char.ToUpper(c) - 'A' + 1;
+                    }
+
+                    List<string> specials = GetSpecialWords();
+                    if (specials != null && specials.Contains(_currentWord, StringComparer.OrdinalIgnoreCase))
+                    {
+                        Debug.WriteLine("Special word entered.");
+                        scoreCounter *= 2;
+                    }
+
+                    MessageBox.Show($"Your score for {_currentWord} is {scoreCounter}.", "Score");
+                } catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                
+                Debug.WriteLine(_currentWord);
+            }
+            
+            return Task.CompletedTask;
         }
     }
 }
